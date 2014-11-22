e706009cdf2d132755489e8f8b61917a679275dc
kirillne <kirillne35@gmail.com>
2014-11-22 22:11:53 +0300

    NewtonsoftXmlJsonConverter

---
+++ NewtonsoftConverter/NewtonsoftJsonXmlConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using AssemblyLoader;
+
+namespace NewtonsoftConverter
+{
+    public class NewtonsoftJsonXmlConverter : IConverter
+    {
+        public string ConvertInput(string sourse)
+        {
+            using (var stringWriter = new StringWriter())
+            using (var xmlTextWriter = XmlWriter.Create(stringWriter))
+            {
+                Newtonsoft.Json.JsonConvert.DeserializeXmlNode(sourse).WriteTo(xmlTextWriter);
+                xmlTextWriter.Flush();
+                return stringWriter.GetStringBuilder().ToString();
+            }
+
+        }
+
+        public string ConvertOutput(string sourse)
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml(sourse);
+            return Newtonsoft.Json.JsonConvert.SerializeXmlNode(doc);
+        }
+    }
+}
